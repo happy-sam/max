a723adf1f46aa6cb40107b9225ffc9b5a1632f3e
Stanisław Romek <info@happy.ovh>
2019-07-01 20:23:25 +0200

    Korekta

--- script.js
+++ script.js
@@ -3,18 +3,6 @@ const max = document.querySelector(".max");
 
 let number = [511, 22, 107, 1005, 13, 50, 10];
 
-arr.textContent = number;
+let maxNumber = Math.max(...number)
 
-let compare = (a, b) => {
-    if (a < b) {
-        return -1
-    } else if (a > b) {
-        return 1
-    } else {
-        return 0
-    }
-};
-
-number.sort(compare);
-
-max.textContent = number[number.length - 1]
+max.textContent = maxNumber;
